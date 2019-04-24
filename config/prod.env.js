2bd79cacc2f9a491734959811da3fe15ffecd064
HappyWednesday <happy.wednesday.kyo@gmail.com>
2019-04-25 02:24:59 +0900

    api-key

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,4 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_ENDPOINT: '"https://happy-wednesday.herokuapp.com/api/1.0/"',
-  HWD_API_TOKEN:'"${process.env.HWD_API_TOKEN}"',
-  HWD_API_SECRET_KEY:'"${process.env.HWD_API_SECRET_KEY}"',
 }
